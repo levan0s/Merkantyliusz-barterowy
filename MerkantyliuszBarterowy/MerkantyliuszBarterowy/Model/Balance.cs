2e089f59f9e1a36ddf89d23b418c81df11adae97
levan0s <nlevanos@gmail.com>
2012-03-10 16:39:20 +0100

    references #17

---
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/Balance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MerkantyliuszBarterowy.Model
+{
+    class Balance
+    {
+        
+        public int Quantity
+        { get; set; }
+        public string BalanceName
+        { get { return _balanceName; } }
+        
+        public Balance(TradeGood tradeGood, int quantity)
+        {
+            Quantity = quantity;
+            _balanceName = tradeGood.Name;
+            _tradeGood = tradeGood;
+        }
+
+        TradeGood _tradeGood;
+        private string _balanceName;
+    }
+}
