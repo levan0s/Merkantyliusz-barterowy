148d31de6391ca3a9600edbcb43f92ef76d45e0d
levan0s <nlevanos@gmail.com>
2012-03-10 16:50:24 +0100

    references #11

--- MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/TradeGood.cs
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/TradeGood.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MerkantyliuszBarterowy.Model.TradeGoods;
 
 namespace MerkantyliuszBarterowy.Model
 {
     class TradeGood
     {
-        public TradeGood(String name, decimal CashEquvalent) 
+        public TradeGood(string name, decimal cashEquvalent, TradeGoodType tradeGoodType) 
         {
-            
+            _cashEquivalent = cashEquvalent;
+            _name = name;
+            _tradeGoodType = tradeGoodType;
         }
+
+        public decimal CashEquivalent
+        {
+            get { return _cashEquivalent; }
+            set { _cashEquivalent = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        private decimal _cashEquivalent;
+        private string _name;
+        private TradeGoodType _tradeGoodType;
     }
+   
 }
