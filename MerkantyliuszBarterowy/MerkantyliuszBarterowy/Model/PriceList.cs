98c4af45bcf8f498137effea68b431dc5c587f78
levan0s <nlevanos@gmail.com>
2012-03-11 12:58:30 +0100

    encja cennika

--- MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/PriceList.cs
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/PriceList.cs
@@ -7,5 +7,48 @@ namespace MerkantyliuszBarterowy.Model
 {
     class PriceList
     {
+        #region public methods
+
+        public string GetTownName()
+        {
+            return _town.Name;
+        }
+        public decimal GetCashEquivalent(string tradeGoodName)
+        {
+            decimal cashEquivalent = 0;
+            foreach (TradeGood trGood in _tradeGood)
+            {
+                if (trGood.Name == tradeGoodName)
+                {
+                    cashEquivalent = trGood.CashEquivalent;
+                }
+            }
+            return cashEquivalent;
+        }
+        public void SetCashEquivalent(string tradeGoodName, decimal cashEquivalent)
+        {
+            foreach (TradeGood trGood in _tradeGood)
+            {
+                if (trGood.Name == tradeGoodName)
+                {
+                    trGood.CashEquivalent = cashEquivalent;
+                }
+            }
+        }
+
+        #endregion
+
+
+        public PriceList(Town town, params TradeGood[] tradeGood )
+        {
+            _town = town;
+            foreach (TradeGood trGood in tradeGood)
+            {
+                _tradeGood.Add(trGood);
+            }
+        }
+
+        private Town _town;
+        private List<TradeGood> _tradeGood = new List<TradeGood>();
     }
 }
