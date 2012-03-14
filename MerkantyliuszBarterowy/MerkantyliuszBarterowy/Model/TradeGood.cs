dd7cb2d21b344dd9102f1194390ccde167bb9ef9
levan0s <nlevanos@gmail.com>
2012-03-14 12:00:31 +0100

    cena usunieta z produktu

--- MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/TradeGood.cs
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/TradeGood.cs
@@ -8,24 +8,17 @@ namespace MerkantyliuszBarterowy.Model
 {
     class TradeGood
     {
-        public TradeGood(string name, decimal cashEquvalent, TradeGoodType tradeGoodType) 
+        public TradeGood(string name, TradeGoodType tradeGoodType) 
         {
-            _cashEquivalent = cashEquvalent;
             _name = name;
             _tradeGoodType = tradeGoodType;
         }
-
-        public decimal CashEquivalent
-        {
-            get { return _cashEquivalent; }
-            set { _cashEquivalent = value; }
-        }
+        
         public string Name
         {
             get { return _name; }
         }
-
-        private decimal _cashEquivalent;
+        
         private string _name;
         private TradeGoodType _tradeGoodType;
     }
