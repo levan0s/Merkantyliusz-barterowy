7b4db014218b1ad1cb029c567324024d086e4152
levan0s <nlevanos@gmail.com>
2012-03-14 11:58:15 +0100

    n/a

---
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/PriceListElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MerkantyliuszBarterowy.Model
+{
+    class PriceListElement
+    {
+        public decimal CashEquivalent { get; set; }
+
+        public string GetName()
+        {
+            return _tradeGood.Name;
+        }
+
+        public PriceListElement(TradeGood tradegood, decimal cashEquivalent)
+        {
+            _tradeGood = tradegood;
+            CashEquivalent = cashEquivalent;
+        }
+
+        private TradeGood _tradeGood;
+    }
+}
