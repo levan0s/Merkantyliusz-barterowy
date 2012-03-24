980a615496393285f467d3f7573de94014b661a1
levan0s <nlevanos@gmail.com>
2012-03-24 15:49:10 +0100

    zrefaktoryzowana statyczna metoda tworzaca cennik; zmiana implementacji cennika: List<> => Dictionary<>

--- MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/PriceList.cs
+++ MerkantyliuszBarterowy/MerkantyliuszBarterowy/Model/PriceList.cs
@@ -7,59 +7,40 @@ namespace MerkantyliuszBarterowy.Model
 {
     class PriceList
     {
-        #region public methods
+        public Dictionary<TradeGood, decimal> PriceListDictionary { get; set; }
         
         public decimal GetCashEquivalent(string tradeGoodName)
         {
-            decimal cashEquivalent = 0;
-            foreach (PriceListElement priceListElement in _priceList)
-            {
-                if (priceListElement.GetName() == tradeGoodName)
-                {
-                    cashEquivalent = priceListElement.CashEquivalent;
-                }
-            }
+            PriceListElement listElement = (PriceListElement)PriceListDictionary.Where(x => x.Key.Name == tradeGoodName);
+            decimal cashEquivalent = listElement.CashEquivalent;
             return cashEquivalent;
         }
 
         public void SetCashEquivalent(string tradeGoodName, decimal cashEquivalent)
         {
-            foreach (PriceListElement priceListElement in _priceList)
-            {
-                if (priceListElement.GetName() == tradeGoodName)
-                {
-                    priceListElement.CashEquivalent = cashEquivalent;
-                }
-            }
+            PriceListElement listElement = (PriceListElement)PriceListDictionary.Where(x => x.Key.Name == tradeGoodName);
+            listElement.CashEquivalent = cashEquivalent;
         }
 
-        public static PriceList defaultPriceList()
+        public static PriceList GetDefaultPriceList()
         { 
-            PriceList defaultPriceList = new PriceList(
-               new PriceListElement(new TradeGood("wood", TradeGoods.TradeGoodType.material), 30),
-               new PriceListElement(new TradeGood("stone", TradeGoods.TradeGoodType.material), 35),
-               new PriceListElement(new TradeGood("steel", TradeGoods.TradeGoodType.material), 40),
-               new PriceListElement(new TradeGood("meat", TradeGoods.TradeGoodType.material), 17),
-               new PriceListElement(new TradeGood("fish", TradeGoods.TradeGoodType.material), 19),
-               new PriceListElement(new TradeGood("cheese", TradeGoods.TradeGoodType.material), 16),
-               new PriceListElement(new TradeGood("apples", TradeGoods.TradeGoodType.material), 10),
-               new PriceListElement(new TradeGood("bread", TradeGoods.TradeGoodType.material), 6),
-               new PriceListElement(new TradeGood("salt", TradeGoods.TradeGoodType.material), 15),
-               new PriceListElement(new TradeGood("wine", TradeGoods.TradeGoodType.material), 20),
-               new PriceListElement(new TradeGood("beer", TradeGoods.TradeGoodType.material), 15),
-               new PriceListElement(new TradeGood("mead", TradeGoods.TradeGoodType.material), 17));
+            List<TradeGood> tradeGoodList = TradeGood.GetDefaultTradeGoodList();
+            Dictionary<TradeGood, decimal> priceList = new Dictionary<TradeGood, decimal>();
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "wood"), 30);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "stone"), 35);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "steel"), 40);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "fish"), 19);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "cheese"), 16);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "apples"), 10);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "bread"), 6);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "salt"), 15);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "wine"), 20);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "beer"), 15);
+            priceList.Add((TradeGood)tradeGoodList.Where(X => X.Name == "mead"), 17);
+
+            PriceList defaultPriceList = new PriceList();
 
             return defaultPriceList;
         }
-
-        #endregion
-
-
-        public PriceList( params PriceListElement[] priceListelements )
-        {
-            _priceList.AddRange(priceListelements);
-        }
-
-        private List<PriceListElement> _priceList = new List<PriceListElement>();
     }
 }
